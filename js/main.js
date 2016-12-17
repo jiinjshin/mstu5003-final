a8f580f2ee908976324b616d2e73255493dc8071
jiin <jiinshin@bearune-jjs.local>
2016-12-17 17:24:46 -0500

    Added content, css, and additional js

--- js/main.js
+++ js/main.js
@@ -1,116 +1,101 @@
 var recipeData = [{
     id: "1a",
     stepNumber: 1,
-    imageUrl: "http://placehold.it/220x220",
-    videoUrl: "",
-    desText: "this is text for step one.",
-    addText: "DEMO TEXT ADDITIONAL"
+    imageUrl: "http://placehold.it/300x220",
+    videoUrl: "http://placehold.it/300x220",
+    desText: "Mix instant pudding mix with sweetened condensed milk and put in the freezer until it is set.",
+    addText: "What happens to instant pudding mix (post-mix) when it is in the fridge (and why it is important to wait)."
 }, {
     id: "2b",
     stepNumber: 2,
-    imageUrl: "",
-    videoUrl: "",
-    desText: "this is text for step two.",
-    addText: ""
+    imageUrl: "http://placehold.it/220x220",
+    videoUrl: "http://placehold.it/220x220",
+    desText: "Whip heavy cream into whipped cream.",
+    addText: "How to prepare whipped cream properly"
 }, {
     id: "3c",
     stepNumber: 3,
-    imageUrl: "",
-    videoUrl: "",
-    desText: "this is text for step three.",
-    addText: ""
+    imageUrl: "http://placehold.it/220x220",
+    videoUrl: "http://placehold.it/220x220",
+    desText: "Fold whipped cream into the pudding mixture until fully incorporated.",
+    addText: "How to fold in pudding and whipped cream mix"
 }, {
     id: "4d",
     stepNumber: 4,
-    imageUrl: "",
-    videoUrl: "",
-    desText: "this is text for step four.",
-    addText: ""
+    imageUrl: "http://placehold.it/220x220",
+    videoUrl: "http://placehold.it/220x220",
+    desText: "Layer pudding mixture, wafers, and sliced bananas into a glass bowl in the specified order. Repeat. Last layer should be the pudding mix.",
+    addText: "What kind of banana is best for optimal banana pudding texture"
 }, {
     id: "5e",
     stepNumber: 5,
-    imageUrl: "",
-    videoUrl: "",
-    desText: "this is text for step five.",
-    addText: ""
+    imageUrl: "http://placehold.it/220x220",
+    videoUrl: "http://placehold.it/220x220",
+    desText: "Cover the glass bowl with foil or saran wrap and let sit in fridge for at least two hours.",
+    addText: "What happens to wafers in the pudding (and why it is important to wait)"
 }, {
     id: "6f",
     stepNumber: 6,
-    imageUrl: "",
-    videoUrl: "",
-    desText: "this is text for step six.",
-    addText: "Bananas"
+    imageUrl: "http://placehold.it/220x220",
+    videoUrl: "http://placehold.it/220x220",
+    desText: "Sprinkle crushed wafers on top of the pudding, and serve!",
+    addText: "Why you need to wait"
 }];
-
-function getById(list, id) {
-    var targetItem = list.find(function(listItem) {
-        return listItem.id === id;
-    });
-    return targetItem;
-}
-
 
 // creating the list of recipe steps
 recipeData.forEach(function(step) {
     var recipeListEl = document.querySelector('#recipe-list');
 
-    // var recipeNumberEl = document.createElement('ul');
-    // recipeNumberEl.id = step.id;
-    // recipeNumberEl.classList.add('recipeNumber');
-    // recipeNumberEl.innerHTML = step.stepNumber;
+    // Creating number
+    var numContainer = document.createElement('span');
+    numContainer.classList.add('number');
+    numContainer.innerText = step.stepNumber;
 
-		// Creating number
-		var numContainer = document.createElement('span');
-		numContainer.classList.add('number');
-		numContainer.innerText = step.stepNumber;
-
-		// Create step item
+    // Create step item
     var stepTextEl = document.createElement('div');
     stepTextEl.id = step.id;
     stepTextEl.classList.add('stepText');
 
-		// Append the span to the div, and then add more to the div.
-		stepTextEl.appendChild(numContainer);
-    stepTextEl.innerHTML = stepTextEl.innerHTML + " --- " +step.desText;
+    // Append the span to the div, and then add more to the div.
+    stepTextEl.appendChild(numContainer);
+    stepTextEl.innerHTML = stepTextEl.innerHTML + step.desText;
 
     // recipeListEl.appendChild(recipeNumberEl);
     recipeListEl.appendChild(stepTextEl);
 
-    // var addListEl = document.querySelector('#add-list');
-    //
-    // var addListTextEl = document.createElement('ul');
-    // addListTextEl.id = step.id;
-    // addListTextEl.classList.add('addText');
-    // addListTextEl.innerHTML = step.addText;
-    //
-    // addListEl.appendChild(addListTextEl);
 });
+
+// active step is highlighted
+
+
 
 // event listener listens to whole listener
 var stepListEl = document.querySelector('#recipe-list');
 stepListEl.addEventListener('click', function(event) {
     console.log(event.target);
+    var dataID = event.target.id;
 
-		var dataID = event.target.id;
+    // Let's ues the ide to search through the data.
+    var foundStep = recipeData.find(function(step) {
+        return step.id === dataID;
+    });
 
-		// Let's ues the ide to search through the data.
-		var foundStep = recipeData.find(function(step){
-			return Math.random() < 0.5;
-		  // return step.id === dataID;
-		});
+    // Prints out the step that matches with the clicked item
+    console.log(foundStep);
 
-		console.log(foundStep);
+    // Create image
+    var rightImage = document.createElement('img');
+    rightImage.src = foundStep.imageUrl;
 
-		document.getElementById('rightContent').innerHTML = foundStep.addText;
+    // Create video
+    var rightVideo = document.createElement('img');
+    rightVideo.src = foundStep.videoUrl;
 
+    // div of right text
+    document.getElementById('rightContent').innerHTML = foundStep.addText;
+    document.getElementById('rightContent').appendChild(rightImage);
+    document.getElementById('rightContent').appendChild(rightVideo);
 
-
-    // if (event.target && event.target.matches(ul.stepNumber)) {
-    //     var targetStep = getById(recipeData, event.target.id);
-    //     displayStep(targetStep);
-    // } else {
-    //     alert('Clicked an element that is not a ul.stepItem');
-    // }
 });
 
 // Just a demo for how to study...
